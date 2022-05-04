48926869e08b713227b94a10466c265cbe0e7f1d
Richard <richardawaddell@yahoo.com>
2022-05-04 10:15:09 -0400

    error 2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const hostname = '127.0.0.1';
-const port = 3333;
+const port = 3000;
 
 const express = require('express'),
     es6Renderer = require('express-es6-template-engine');
